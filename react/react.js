fd47536b65c262bfd4e6e1cdecdfd800b54f05c3
zhaopeng16 <zhaopeng16@meituan.com>
2018-12-15 13:23:05 +0800

    createElement 添加type dom children

--- react/react.js
+++ react/react.js
@@ -5,6 +5,22 @@
         return anElement;
     }
 
+    function anElement(element, children) {
+        if (typeof (element) === 'function') {
+            return element();
+        } else {
+            const anElement = document.createElement(element);
+            children.forEach(child => {
+                if (typeof (child) === 'object') {
+                    anElement.appendChild(child);
+                } else {
+                    anElement.innerHTML += child;
+                }
+            });
+            return anElement;
+        }
+    }
+
     function createElement(el, props, ...children) {
         return anElement(el, children);
     }
@@ -17,4 +33,4 @@
             domEl.appendChild(el);
         }
     };
-})();   
+})();
